d4f1283fcacfc309e0459968cb0570a3eb2de77e
Jace Browning <jacebrowning@gmail.com>
2019-02-05 23:32:27 -0500

    WIP: Preserve formatting in inferred files

--- tests/test_file_inference.py
+++ tests/test_file_inference.py
@@ -1,7 +1,7 @@
 from datafiles import auto
 
 
-def test_auto(write, logbreak, expect):
+def test_auto_with_sample_file(write, logbreak, expect, read, dedent):
 
     write(
         'tmp/sample.yml',
@@ -28,7 +28,35 @@ def test_auto(write, logbreak, expect):
     expect(sample.heterogeneous_list) == [1, 'abc']
     expect(sample.empty_list) == []
 
+    logbreak("Updating attribute")
+    sample.homogeneous_list.append(3.4)
+    sample.heterogeneous_list.append(5.6)
+    sample.empty_list.append(7.8)
 
-def test_auto_travis(expect):
+    logbreak("Reading file")
+    expect(read('tmp/sample.yml')) == dedent(
+        """
+
+        homogeneous_list:
+        - 1
+        - 2
+        - 3
+
+        heterogeneous_list:
+        - 1
+        - abc
+        - 5.6
+
+        empty_list:
+        - 7.8
+
+        """
+    )
+
+
+def test_auto_with_real_files(expect):
     travis = auto('.travis.yml')
     expect(travis.language) == 'python'
+
+    mkdocs = auto('mkdocs.yml')
+    expect(mkdocs.theme) == 'readthedocs'
